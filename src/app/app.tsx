bc7252e53fde8324bd25708868102d33247d88c2
Damian <ruiz.damian17@gmail.com>
2023-10-18 12:54:55 -0700

    Board component rendered in App

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
+import Board from './components/Board/Board';
+import useBoard from './hooks/useBoard/useBoard';
 
 export function App() {
+
+  const board = useBoard()
+
   return (
     <>
-      <NxWelcome title="checkers" />
-
-      <div />
+      <Board board={board} />
     </>
   );
 }
