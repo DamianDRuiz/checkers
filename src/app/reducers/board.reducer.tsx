3cfeeb7ce1620698f890562de7a5a517df2e564b
Damian <ruiz.damian17@gmail.com>
2023-10-18 17:12:47 -0700

    Select action reworked

--- src/app/reducers/board.reducer.tsx
+++ src/app/reducers/board.reducer.tsx
@@ -1,10 +1,22 @@
 import { Board as BoardType } from '../types/Board';
+import { Square } from '../types/Square';
 
 export function reducer(state: BoardType, action: any) {
   switch (action.type) {
     case 'select':
-      state.squares[action.id].selected = true;
+      state.squares = select(state.squares, action.id);
       return { ...state };
       break;
   }
+}
+
+function select(squares: Square[], id: number) {
+  const currentlySelectedSquare = squares.find(
+    (square) => square.selected == true
+  );
+
+  if (currentlySelectedSquare) currentlySelectedSquare.selected = false;
+
+  squares[id].selected = true;
+  return squares;
 }
